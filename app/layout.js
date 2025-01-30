f8e4f1a2f863c1f407dae05e215646f0fdad94ab
AKASHHARIM <167068172+AKASHHARIM@users.noreply.github.com>
2025-01-30 23:41:03 +0530

    dark mode functionality

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "../components/Header";
+import ThemeComp from "@/components/ThemeComp"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,12 +21,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeComp>
         <Header/>
         {children}
+
+        </ThemeComp>
       </body>
     </html>
   );
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,10 @@
+'use client';
+
 import React from 'react'
+
+
 import Link from 'next/link'
+import DarkModeSwitch from './DarkmodeSwitch'
 function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
@@ -14,12 +19,15 @@ function Header() {
         <Link href={'/about'}>About</Link>
       </li>
     </ul>
+    <div className='flex items-center gap-4'>
+           <DarkModeSwitch/>
     <Link href={'/'} className='flex gap-1 items-center'>
       <span className='text-2xl font-bold bg-blue-400 py-1 px-2 rounded-lg'>
         MOVIEZZ
       </span>
       <span className='text-xl hidden sm:inline'>Corner</span>
     </Link>
+    </div>
   </div>  )
 }
 
